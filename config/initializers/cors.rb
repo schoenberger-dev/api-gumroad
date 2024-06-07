16dc3bc27c62eebeba59133fa73be703a6a2d6d3
niklas schoenberger <001@schoenberger.dev>
2024-06-07 16:43:22 +0200

    chore(cors): include vercel deployment urls to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Specify which origins are allowed to make CORS requests
-    origins 'localhost:3000', 'heygumroad.schoenberger.dev', 'heygumroad.schoenberger.dev'
+    origins 'localhost:3000', 'gumroad.schoenberger.dev', %r{/https://gumroad-(.*)\.vercel\.app/}
 
     # Specify which resources are allowed and which methods and headers are supported
     resource '/api/*',
